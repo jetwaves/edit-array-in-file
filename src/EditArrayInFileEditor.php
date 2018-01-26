53d27f54a4fb9ad27dfbf1b517ebd1bfa332b822
jetwaves@office <jetwaves@office>
2018-01-26 20:03:37 +0800

    Now we can split target file (of type Array and variable array ) into 3 parts:  - code lines before edit area,  - edit area,  => it is the array to be edited itself  - code lines after edit area.

--- src/EditArrayInFileEditor.php
+++ src/EditArrayInFileEditor.php
@@ -16,13 +16,21 @@ class Editor {
     private $_codesBeforeEditArea = [];
     private $_codesAfterEditArea = [];
     private $_editArea = [];
+    private $_res = [];
 
-    const TYPE_RAW = 0;                 //  simple and headless str_pos
-    const TYPE_VARIABLE = 1;        //  change
-    const TYPE_KV_PAIR = 2;
+    const TYPE_RAW = 0;                                                 //  simple and headless str_pos
+    const TYPE_VARIABLE = 1;                                        //  a variable of array.
+    const TYPE_ARRAY_IN_VARIABLE_VALUE = 2;     //  a value array in a variable
+                            //          eg.   $variable_lv1 = [  'key1_lv1' => [ 'targetKey' => 'targetValueToEdit', 'key2_lv2' => '...val_lv2' ], 'key2_lv1' => '....val2_lv2'  ] ;
+    const TYPE_KV_PAIR = 3;                                         // simple kv pair
+    const TYPE_ARRAY_IN_KV_PAIR = 4;                    // multi_level_kv_pair
+                            //         eg.   'some_key' =>   [  'key1_lv1' => [ 'targetKey' => 'targetValueToEdit', 'key2_lv2' => '...val_lv2' ], 'key2_lv1' => '....val2_lv2'  ] ;
+    const TYPE_EOB_COMMA = 5;                               //    ],   end of bloc
+    const TYPE_EOB_SEMI_COLON = 5;                               //    ],   end of bloc
 
     const INSERT_TYPE_DATA_ONLY = 0;
     const INSERT_TYPE_KV_PAIR = 0;
+
 
     function __construct($filename = null)
     {
@@ -36,14 +44,62 @@ class Editor {
     private function parse(){
         $this->_contentArray = file($this->_filleFullName);
     }
-    
-    public function where($arrayName, $type = self::TYPE_VARIABLE)
+
+    public function where($arrayName, $options = [], $type = self::TYPE_VARIABLE)
     {
-        foreach ($this->_contentArray as $line ) {
+        echo '  arrayName = '.$arrayName.PHP_EOL;
+        $this->_codesBeforeEditArea = [];
+        $this->_codesAfterEditArea = [];
+        $foundStart = false;
+        $foundEnd = false;
+        $eob = false;                                 // end of target code bloc   (    ];   or  ],  )
+        foreach ($this->_contentArray as $ln => $originalLine ) {
+            $line = trim($originalLine);
+//            if($line == '') continue;           // ignore empty lines;
+            if($foundStart == false){
+                list($foundStart, $eob) = $this->match($line, $arrayName, $type);
+                $this->_codesBeforeEditArea[] = $originalLine;
+            }
+            if($foundStart && $foundEnd == false){
+                list($foundEnd, $eob) = $this->match($line, $eob, $type, true);
+                $this->_editArea[] = $originalLine;
+            }
+            if($foundStart && $foundEnd){
+                $this->_codesAfterEditArea[] = $originalLine;
+            }
 
+            $parseInfo =
+                'ln         = '.$ln.PHP_EOL.
+                '               line       = '.$line.PHP_EOL.
+                '               pos        = '.strpos($originalLine, "aliases").PHP_EOL.
+                '               foundStart = '.($foundStart?'true':'false').PHP_EOL.
+                '               foundEnd   = '.($foundEnd?'true':'false').PHP_EOL.
+                '               eob        = '.($eob?'true':'false').PHP_EOL.PHP_EOL;
+            $this->_res[] = $parseInfo;
+//            echo print_r($parseInfo, true).PHP_EOL;
         }
+        echo '  ================== '.PHP_EOL;
+//        echo print_r($this->_res, true);
+//        var_dump($this->_res);
+        array_pop($this->_codesBeforeEditArea);
+        array_splice($this->_codesAfterEditArea, 0, 1);
+        return $this;
+    }
 
-        echo print_r($this->_contentArray);
+    // get the content of the founded array.
+    public function get(){
+        echo '_codesBeforeEditArea      '.PHP_EOL;
+        echo print_r($this->_codesBeforeEditArea, true);
+
+        echo '_editArea     '.PHP_EOL;
+        echo print_r($this->_editArea, true);
+
+
+        echo '  _codesAfterEditArea     '.PHP_EOL;
+        echo print_r($this->_codesAfterEditArea, true);
+
+
+        return $this->_editArea;
     }
 
     public function find(){
@@ -59,18 +115,54 @@ class Editor {
 
     }
 
-    private function match($line, $keyword, $type ){
-            switch($type){
-                case self::TYPE_VARIABLE :
-                    //  protected $middleware = [
-                    //  protected $routeMiddleware = [
+    private function match($line, $keyword, $type, $matchEndOfBloc = null ){
+        $endOfBlock = '';
+        switch($type){
+            case self::TYPE_VARIABLE :
+                //  protected $middleware = [  ];
+                //  protected $routeMiddleware = [ ];
+                $endOfBlock = '];';
+                if(!$matchEndOfBloc){
+                    $keyword = ' '.$keyword." = [";
+                    $pos = self::contains($line, $keyword);
+                    if($pos){
+                        return [true, $endOfBlock];
+                    }
+                } else {
+                    if(substr($line,0,1)=="]" && substr($line,1, 1) ==";"){
+                        return [true, true];
+                    }
+                }
+                break;
+            case self::TYPE_KV_PAIR:
+                // 'providers' => [  ],
 
-                    break;
-                case self::TYPE_KV_PAIR:
+                $endOfBlock = '],';
+                if(!$matchEndOfBloc){
+                    $keyword = "'".$keyword."' => [";
+                    $pos = self::contains($line, $keyword);
+                    if($pos){
+                        return [true, $endOfBlock];
+                    }
+                } else {
+                    if(substr($line,0,1)=="]" && substr($line,1, 1) ==","){
+                        return [true, true];
+                    }
+                }
+//                echo print_r(['line' => $line, 'keyword' => $keyword,   'pos' => $pos?'true': 'false']);
+                break;
+            case self::TYPE_ARRAY_IN_KV_PAIR:
+                // 'providers' => [  ],
 
-                    break;
-            }
-        return false;
+                $endOfBlock = '],';
+                break;
+            case self::TYPE_ARRAY_IN_VARIABLE_VALUE:
+                // 'providers' => [  ],
+
+                $endOfBlock = '];';
+                break;
+        }
+        return [false, false];
     }
 
     // write the edited content back to the source file
@@ -78,6 +170,18 @@ class Editor {
 
     }
 
+
+
+    public static function contains($haystack, $needles)
+    {
+        foreach ((array) $needles as $needle) {
+            if ($needle !== '' && mb_strpos($haystack, $needle) !== false) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 
 
 }
