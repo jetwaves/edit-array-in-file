89cfaae2fd47e97c96dea4dfd54b6d6a5bb95aa0
jetwaves@office <jetwaves@office>
2018-02-12 17:06:45 +0800

    refactor test cases.

--- src/Test/test.php
+++ src/Test/test.php
@@ -1,35 +1,43 @@
 <?php
-namespace Jetwaves\EditArrayInFile;
+namespace Jetwaves\EditArrayInFile\Test;
 
-require('EditArrayInFileEditor.php');
+
+require('../EditArrayInFileEditor.php');
+use Jetwaves\EditArrayInFile\Editor as Editor;
+
 
 //###When the target is an array in the value of a Key-Value pair)
 // ======= exemple of insert one line into [aliases] array in file [app.php]
-$editor = new Editor('testSource/app.php');
+$editor = new Editor('../testSource/app.php');
 
 // 1.1. CRUD->R : Find target variable in a php source code file:
-//    $targetArray = $editor->where('aliases',[], Editor::TYPE_KV_PAIR)->get();
-//    echo '     '.__method__.'() line:'.__line__.'   $targetArray  = '.print_r($targetArray, true);
+
+    echo ''.__FILE__.'->'.__method__.'() line:'.__line__.'   1.1. CRUD->R : Find target variable "aliases" in a php source code file: '.PHP_EOL;
+    $targetArray = $editor->where('aliases',[], Editor::TYPE_KV_PAIR)->get();
+    echo ''.__FILE__.'->'.__method__.'() line:'.__line__.'   $targetArray  = '.print_r($targetArray, true);
 
 
 // 1.2. CRUD->C : Insert into target Key-Value pair in a php source code file:
-//    $editor->where('aliases',[], Editor::TYPE_KV_PAIR);
-//    $editor->insert("'JWTAuth' => Tymon\\JWTAuth\\Facades\\JWTAuth::class".PHP_EOL);
-//    $editor->save()->flush();
+    echo ''.__FILE__.'->'.__method__.'() line:'.__line__.'   1.2. CRUD->C : Insert into target Key-Value pair in a php source code file: '.PHP_EOL;
+    $editor->where('aliases',[], Editor::TYPE_KV_PAIR);
+    $editor->insert("'JWTAuth007' => Tymon\\JWTAuth\\Facades\\JWTAuth008::class".PHP_EOL);
+    $editor->save()->flush();
 
+
+    return;
 // 1.3. CRUD->R : Search in target Key-Value pair for a key :
-//    $val = $editor->where('aliases',[], Editor::TYPE_KV_PAIR)
-//                    ->find('App', Editor::FIND_TYPE_KEY_ONLY);
-//    echo '     '.__method__.'() line:'.__line__.'   target key `s val  = '.print_r($val, true);
-//
-//    // 1.3  case 2
-//    $val = $editor->where('providers',[], Editor::TYPE_KV_PAIR)
-//        ->find('Illuminate\Bus\BusServiceProvider::class', Editor::FIND_TYPE_ALL);
-//    echo '     '.__method__.'() line:'.__line__.'   target key `s val  = '.print_r($val, true);
+    $val = $editor->where('aliases',[], Editor::TYPE_KV_PAIR)
+                    ->find('App', Editor::FIND_TYPE_KEY_ONLY);
+    echo '     '.__method__.'() line:'.__line__.'   target key `s val  = '.print_r($val, true);
+
+    // 1.3  case 2
+    $val = $editor->where('providers',[], Editor::TYPE_KV_PAIR)
+        ->find('Illuminate\Bus\BusServiceProvider::class', Editor::FIND_TYPE_ALL);
+    echo '     '.__method__.'() line:'.__line__.'   target key `s val  = '.print_r($val, true);
 // 1.4. CRUD->D : delete one line in target Key-Value pair
-//        $editor->where('aliases',[], Editor::TYPE_KV_PAIR)
-//               ->find('JWTAuth', Editor::FIND_TYPE_ALL);
-//        $editor->delete()->save()->flush();
+        $editor->where('aliases',[], Editor::TYPE_KV_PAIR)
+               ->find('JWTAuth', Editor::FIND_TYPE_ALL);
+        $editor->delete()->save()->flush();
 // 1.5. CRUD->C : add one line after / before the anchor line
 
 
